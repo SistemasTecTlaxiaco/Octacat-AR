5531b2c975ecc27a508842b4d7f4f97b29edf0e7
Jaci14 <79876660+Jaci14@users.noreply.github.com>
2022-03-16 20:06:42 -0600

    App AR octa

--- index.php
+++ index.php
@@ -1,16 +1,26 @@
 <!DOCTYPE html>
-<html>  
+<html>
   <head>
-    <script src="https://aframe.io/releases/0.8.0/aframe.min.js"></script> 
-    <script src="https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js"></script>
-  </head>   
-
+    <script src="https://aframe.io/releases/1.1.0/aframe.min.js"></script>
+    <script src="https://libs.zappar.com/zappar-aframe/0.3.6/zappar-aframe.js"></script>
+    <title>Octacat</title>
+  </head>
   <body>
-    <a-scene embedded arjs>
-      <a-entity scale=".1 .1 .1"> 
-        <a-entity gltf-model="\Asssets\octo.glb"  scale="0.1 0.1 0.1" position="0 0 0" crossOrigin="anonymous">
+    <a-scene>
+      <a-entity zappar-permissions-ui id="permissions"></a-entity>
+      <a-entity zappar-compatibility-ui id="compatibility"></a-entity>
+      <a-camera zappar-camera></a-camera>
+      <a-entity
+        zappar-image="target:https://cdn.glitch.global/b44ba1e4-793c-46e0-8aa8-e555b363ca1f/github-logo.zpt?v=1647464696922"
+      >
+        <a-entity
+          id="model"
+          gltf-model="https://cdn.glitch.global/b44ba1e4-793c-46e0-8aa8-e555b363ca1f/octo.glb?v=1647446060850"
+          position="0 0 0"          
+          scale="0.05 0.05 0.05"
+        >
         </a-entity>
-        <a-marker-camera preset='hiro'></a-marker-camera>
-        </a-scene>
-      </body>
-    </html>
+      </a-entity>
+    </a-scene>
+  </body>
+</html>
